483d3f1e31d015bcbfb8354c7aaaf141cbe521f9
freshefisch <62943210+freshefisch@users.noreply.github.com>
2025-08-20 16:20:12 +0200

    first commit, basic site

---
+++ _next/static/chunks/1bb129f7d727aaf8.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/9ea0626d010fa9a5.js",
+  "static/chunks/turbopack-c16c8a623bc852a7.js"
+])
---
+++ _next/static/chunks/321547b71bedb901.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/9ea0626d010fa9a5.js",
+  "static/chunks/turbopack-0ffce556fe8e7ceb.js"
+])
